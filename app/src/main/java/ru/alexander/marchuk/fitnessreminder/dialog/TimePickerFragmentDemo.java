a86b2500f3ff7284cc5bcff9f9d4135c3291c21a
alextula26 <alex_marc86@mail.ru>
2017-06-26 23:10:30 +0300

    User 2

---
+++ app/src/main/java/ru/alexander/marchuk/fitnessreminder/dialog/TimePickerFragmentDemo.java
@@ -0,0 +1,9 @@
+package ru.alexander.marchuk.fitnessreminder.dialog;
+
+import android.support.v4.app.DialogFragment;
+
+public class TimePickerFragment extends DialogFragment {
+	
+	private String DEMO = "DEMO USER 2";
+
+}
