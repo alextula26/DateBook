2539c1baab03b0a6fb517b68b510ead9b02bd19e
alextula26 <alex_marc86@mail.ru>
2017-06-27 22:14:58 +0300

    Commit for SmartGit

--- app/src/main/java/ru/alexander/marchuk/fitnessreminder/dialog/TimePickerFragment.java
+++ app/src/main/java/ru/alexander/marchuk/fitnessreminder/dialog/TimePickerFragment.java
@@ -2,6 +2,13 @@ package ru.alexander.marchuk.fitnessreminder.dialog;
 
 import android.support.v4.app.DialogFragment;
 
+import ru.alexander.marchuk.fitnessreminder.model.Exercise;
+
 public class TimePickerFragment extends DialogFragment {
+
+    public int a;
+    public int b;
+
+    Exercise mExercise = new Exercise();
 
 }
